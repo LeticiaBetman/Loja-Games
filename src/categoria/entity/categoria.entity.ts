6da91efed7b05c2a8ce4710eb540fb7f5a89e645
Leticia Betman <leticiarodriguesbetman@gmail.com>
2025-08-05 22:13:37 -0300

    cria entiity da categoria

---
+++ src/categoria/entity/categoria.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Produto } from '../../produto/entity/produto.entity';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @Column({ length: 255, nullable: false })
+  descricao: string;
+
+  @OneToMany(() => Produto, produto => produto.categoria)
+  produtos: Produto[];
+}
